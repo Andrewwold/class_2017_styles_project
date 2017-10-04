b90279cd49171db033d8449ea9d042327c66a8e8
Andrew <goldsabre@gmail.com>
2017-10-04 13:11:03 -0600

    added scaffold for blogs

---
+++ db/migrate/20171004191038_create_blogs.rb
@@ -0,0 +1,12 @@
+class CreateBlogs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :blogs do |t|
+      t.string :author
+      t.string :title
+      t.text :body
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
